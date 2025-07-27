e62ad2ede23bafc2f7f8f5ded3d80f62366e174b
oluwapelumi11 <oluwapelumiezekiel11@gmail.com>
2025-07-27 15:37:07 +0100

    chnagerd name

--- .next/static/chunks/webpack.js
+++ .next/static/chunks/webpack.js
@@ -192,7 +192,7 @@
 /******/ 	
 /******/ 	/* webpack/runtime/getFullHash */
 /******/ 	!function() {
-/******/ 		__webpack_require__.h = function() { return "43a1d422ef555308"; }
+/******/ 		__webpack_require__.h = function() { return "983ed4ae558e852a"; }
 /******/ 	}();
 /******/ 	
 /******/ 	/* webpack/runtime/global */
